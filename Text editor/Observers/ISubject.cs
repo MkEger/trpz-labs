d624643fb4e4b5baf14d89971cac5b4799d9a72a
Korniichuk Maksym <145021810+MkEger@users.noreply.github.com>
2025-11-22 01:09:01 +0200

    Add ISubject interface for observer pattern

---
+++ Text editor/Observers/ISubject.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TextEditor.Observers
+{
+    public interface ISubject
+    {
+        void Attach(IObserver observer);
+        void Detach(IObserver observer);
+        void Notify();
+    }
+}
